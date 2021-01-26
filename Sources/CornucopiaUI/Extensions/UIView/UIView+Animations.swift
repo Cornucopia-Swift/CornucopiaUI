2502e818bb262292e0c06b2ec641040f73d5f3e8
Dr. Michael Lauer <mickey@vanille-media.de>
2021-01-26 17:24:47 +0100

    UIView+Animations: add CC_bounceOnce

--- Sources/CornucopiaUI/Extensions/UIView/UIView+Animations.swift
+++ Sources/CornucopiaUI/Extensions/UIView/UIView+Animations.swift
@@ -5,6 +5,9 @@ import UIKit.UIView
 
 public extension UIView {
 
+    private static let AnimationBounceKey = "bounce"
+    private static let AnimationKeyPathScale = "transform.scale"
+
     func CC_animateContentChangeOnMainLayerByFading(with duration: TimeInterval = 0.33) {
         let animation = CATransition()
         animation.isRemovedOnCompletion = true
@@ -13,6 +16,16 @@ public extension UIView {
         animation.timingFunction = CAMediaTimingFunction(name: CAMediaTimingFunctionName.easeInEaseOut)
         animation.fillMode = CAMediaTimingFillMode(rawValue: "extended")
         self.layer.add(animation, forKey: nil)
+    }
+
+    func CC_bounceOnce() {
+        guard self.layer.animation(forKey: Self.AnimationBounceKey) == nil else { return }
+        let animation = CABasicAnimation(keyPath: Self.AnimationKeyPathScale)
+        animation.fromValue = 1.0
+        animation.toValue = 1.2
+        animation.autoreverses = true
+        animation.duration = 0.15
+        self.layer.add(animation, forKey: Self.AnimationBounceKey)
     }
 
 }
