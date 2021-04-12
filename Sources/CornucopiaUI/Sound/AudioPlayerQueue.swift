c92669aebdd440f4342dbc2b8eddf55cbfd7524e
Dr. Michael Lauer <mickey@vanille-media.de>
2021-04-12 14:18:28 +0200

    add AudioPlayerQueue

---
+++ Sources/CornucopiaUI/Sound/AudioPlayerQueue.swift
@@ -0,0 +1,38 @@
+//
+//  Cornucopia – (C) Dr. Lauer Information Technology
+//
+#if canImport(AVFoundation)
+import AVFoundation
+
+public class AudioPlayerQueue: NSObject {
+
+    var player: AVAudioPlayer?
+    var urls: [URL] = []
+
+    public static var `default` = AudioPlayerQueue()
+    override private init() { }
+
+    public func play(url: URL) {
+        precondition(Thread.isMainThread, "This API is not thread-safe. Please call it from the main thread instead.")
+
+        guard let _ = player else {
+            guard let player = try? AVAudioPlayer(contentsOf: url) else { return }
+            self.player = player
+            player.delegate = self
+            player.play()
+            return
+        }
+        self.urls.append(url)
+    }
+}
+
+extension AudioPlayerQueue: AVAudioPlayerDelegate {
+
+    public func audioPlayerDidFinishPlaying(_ player: AVAudioPlayer, successfully flag: Bool) {
+        self.player = nil
+        guard !self.urls.isEmpty else { return }
+        self.play(url: self.urls.removeFirst())
+    }
+}
+
+#endif
