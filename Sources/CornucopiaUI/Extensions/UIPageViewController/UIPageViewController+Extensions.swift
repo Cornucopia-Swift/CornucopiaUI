432b1a0c4c9fe181e9fd30aa65dbe516b69d7862
Dr. Michael Lauer <mickey@vanille-media.de>
2021-05-17 09:11:04 +0200

    UIPageViewController+Extensions: also allow _setting_ the currently visible controller

--- Sources/CornucopiaUI/Extensions/UIPageViewController/UIPageViewController+Extensions.swift
+++ Sources/CornucopiaUI/Extensions/UIPageViewController/UIPageViewController+Extensions.swift
@@ -7,8 +7,17 @@ import UIKit.UIPageViewController
 public extension UIPageViewController {
 
     var CC_visibleViewController: UIViewController? {
-        return self.viewControllers?.first
+        /// Return the visible view controller.
+        get {
+            self.viewControllers?.first
+        }
+        /// Set the (initially) visible view controller, calling the delegate method after completion.
+        set {
+            guard let vc = newValue else { return }
+            self.setViewControllers([vc], direction: .forward, animated: false) { _ in
+                self.delegate?.pageViewController?(self, didFinishAnimating: true, previousViewControllers: [], transitionCompleted: true)
+            }
+        }
     }
-
 }
 #endif
