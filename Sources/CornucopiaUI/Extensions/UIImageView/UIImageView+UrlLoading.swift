030c3ba49b07e011e37d3cd2ad8b1feecdb2b622
Dr. Michael Lauer <mickey@vanille-media.de>
2021-05-17 09:07:32 +0200

    UIImageView+UrlLoading: allow specifying a placeholder for when loading fails
    The placeholder gets applied when a) loading the network resource fails or b)
    the delivered network result is not a valid image

--- Sources/CornucopiaUI/Extensions/UIImageView/UIImageView+UrlLoading.swift
+++ Sources/CornucopiaUI/Extensions/UIImageView/UIImageView+UrlLoading.swift
@@ -7,13 +7,16 @@ import UIKit.UIImageView
 
 public extension UIImageView {
 
-    func CC_load(from url: URL) {
+    func CC_load(from url: URL, placeholder: UIImage? = nil) {
 
         let request = URLRequest(url: url)
         Cornucopia.Core.HTTPNetworking().GET(with: request) { (result: Cornucopia.Core.HTTPResponse<Data>) in
-            guard case let .success(code, data) = result, code == .OK else { return }
-            guard let image = UIImage(data: data) else { return }
             DispatchQueue.main.async {
+                if let placeholder = placeholder {
+                    self.image = placeholder
+                }
+                guard case let .success(code, data) = result, code == .OK else { return }
+                guard let image = UIImage(data: data) else { return }
                 self.image = image
             }
         }
