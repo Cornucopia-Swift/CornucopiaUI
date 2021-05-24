7b04972cd4b3f1e8111cfe6cdbec0c136de512bc
Dr. Michael Lauer <mickey@vanille-media.de>
2021-05-24 11:25:14 +0200

    UIImageView+UrlLoading: offload image decoding into a seperate thread

--- Sources/CornucopiaUI/Extensions/UIImageView/UIImageView+UrlLoading.swift
+++ Sources/CornucopiaUI/Extensions/UIImageView/UIImageView+UrlLoading.swift
@@ -9,16 +9,17 @@ public extension UIImageView {
 
     func CC_load(from url: URL, placeholder: UIImage? = nil) {
 
+        let setPlaceholderIfNecessary = {
+            if let placeholder = placeholder {
+                DispatchQueue.main.async { self.image = placeholder }
+            }
+        }
+
         let request = URLRequest(url: url)
         Cornucopia.Core.HTTPNetworking().GET(with: request) { (result: Cornucopia.Core.HTTPResponse<Data>) in
-            DispatchQueue.main.async {
-                if let placeholder = placeholder {
-                    self.image = placeholder
-                }
-                guard case let .success(code, data) = result, code == .OK else { return }
-                guard let image = UIImage(data: data) else { return }
-                self.image = image
-            }
+            guard case let .success(code, data) = result, code == .OK else { return setPlaceholderIfNecessary() }
+            guard let image = UIImage.CC_predecodedImage(data) else { return setPlaceholderIfNecessary() }
+            DispatchQueue.main.async { self.image = image }
         }
     }
 }
