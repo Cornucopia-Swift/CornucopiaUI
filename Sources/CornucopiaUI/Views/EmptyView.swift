5b8539a0efed24d19a1d1a259ba4bb801579c698
Dr. Michael Lauer <mickey@vanille-media.de>
2021-07-14 11:45:58 +0200

    EmptyView: allow advanced configuration

--- Sources/CornucopiaUI/Views/EmptyView.swift
+++ Sources/CornucopiaUI/Views/EmptyView.swift
@@ -8,33 +8,37 @@ import UIKit.UIView
 /// A decorative view for when there is no actual content.
 public class CC_EmptyView: UIView {
 
-    static let PrimaryAlpha: CGFloat = 0.65
-    static let SecondaryAlpha: CGFloat = 0.6
-    static let IconScaling: CGFloat = 0.35
-
+    /// Construction via unarchiving not supported yet
     public required init(coder: NSCoder) {
         fatalError()
     }
 
-    public init(icon: UIImage? = nil, title: String? = nil, subtitle: String? = nil) {
+    /// Create using an `icon`, a `title`, a `subtitle`.
+    /// Further customization of scaling, textStyles, and alpha values is available.
+    /// While the default values are good for full-screen empty views, you might want to adjust them for partial coverage.
+    public init(icon: UIImage? = nil,
+                title: String? = nil,
+                subtitle: String? = nil,
+                titleStyle: UIFont.TextStyle = .title1,
+                subtitleStyle: UIFont.TextStyle = .title3,
+                iconScaling: CGFloat = 0.35,
+                primaryAlpha: CGFloat = 0.65,
+                secondaryAlpha: CGFloat = 0.6
+    ) {
         super.init(frame: .zero)
 
         let imageView = UIImageView(image: icon)
         imageView.contentMode = .scaleAspectFit
-        imageView.alpha = Self.SecondaryAlpha
+        imageView.alpha = secondaryAlpha
 
         let titleLabel = UILabel.CC_init()
-        titleLabel.alpha = Self.PrimaryAlpha
-        #if !os(tvOS)
-        titleLabel.font = UIFont.preferredFont(forTextStyle: .largeTitle)
-        #else
-        titleLabel.font = UIFont.preferredFont(forTextStyle: .headline)
-        #endif
+        titleLabel.alpha = primaryAlpha
+        titleLabel.font = UIFont.preferredFont(forTextStyle: titleStyle)
         titleLabel.text = title
 
         let subtitleLabel = UILabel.CC_init()
-        subtitleLabel.alpha = Self.SecondaryAlpha
-        subtitleLabel.font = UIFont.preferredFont(forTextStyle: .title3)
+        subtitleLabel.alpha = secondaryAlpha
+        subtitleLabel.font = UIFont.preferredFont(forTextStyle: subtitleStyle)
         subtitleLabel.text = subtitle
 
         let stackView = UIStackView(arrangedSubviews: [imageView, titleLabel, subtitleLabel])
@@ -47,8 +51,8 @@ public class CC_EmptyView: UIView {
         NSLayoutConstraint.activate([
             stackView.widthAnchor.constraint(equalTo: self.widthAnchor),
             stackView.centerYAnchor.constraint(equalTo: self.centerYAnchor),
-            imageView.heightAnchor.constraint(equalTo: self.heightAnchor, multiplier: Self.IconScaling),
-            imageView.widthAnchor.constraint(equalTo: self.heightAnchor, multiplier: Self.IconScaling),
+            imageView.heightAnchor.constraint(equalTo: self.heightAnchor, multiplier: iconScaling),
+            imageView.widthAnchor.constraint(equalTo: self.heightAnchor, multiplier: iconScaling),
         ])
     }
 }
